62861163cffda6e8ec47cfddf4cb58ef60fc02ec
SilviaAlconGarrido <alcongarridosilvia@gmail.com>
2023-11-17 20:47:53 +0100

    continue

--- main.py
+++ main.py
@@ -1,50 +1,45 @@
-##############################################################################
-#                                                                            #                       
-#   argparse — Parser for command-line options, arguments and sub-commands   #     
-#                                                                            #
-#   Ironhack Data Part Time --> Sep-2023                                    #
-#                                                                            #
-##############################################################################
-
-
-# import library
-
 import argparse
 
 
 # Script functions 
 
-def enter_number(message):
-    return float(input(message))
+def bicimad(c: str, dest: str):
+    c = input('Elige entre Bicimad o Bicipark: ')
+    dest = input('Introduce un Edificio de carácter monumental: ')
+    print(f'Elegiste: {c}')
+    print(f'En el lugar: {dest}')
 
-def sum_function(x1, x2):
-    return x1 + x2
-    
-def multiply_function(x1, x2):
-    return x1 * x2
+def bicipark(c: str, dest: str):
+    c = input('Elige entre Bicimad o Bicipark: ')
+    dest = input('Introduce un Edificio de carácter monumental: ')
+    print(f'Elegiste: {c}')
+    print(f'En el lugar: {dest}')
     
 
 # Argument parser function
-
-def argument_parser():
-    parser = argparse.ArgumentParser(description= 'Application for arithmetic calculations' )
-    help_message ='You have two options. Option 1: "mult" performs multiplication of two given numbers. Option 2: "sum" performs the sum of two given numbers' 
-    parser.add_argument('-f', '--function', help=help_message, type=str)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description= 'Esta es una aplicación para encontrar una estación de bicimad o bicipark dependiendo del Edificios de carácter monumental en el que estes.' )
+    help_message ='Debes introducir un Edificios de carácter monumental de Madrid'
+    help_category ='Elige entre Bicimad o Bicipark'
+    parser.add_argument('-c', '--category', help=help_message, type=str)
     args = parser.parse_args()
-    return args
+    #return args.e
 
 
 # Pipeline execution
 
-if __name__ == '__main__':
-    if argument_parser().function == 'mult':
-        n1 = enter_number('Enter a number: ')
-        n2 = enter_number('Enter another number: ')
-        result = multiply_function(n1, n2)
-    elif argument_parser().function == 'sum':
-        n1 = enter_number('Enter a number: ')
-        n2 = enter_number('Enter another number: ')
-        result = sum_function(n1, n2)
+#if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description= 'Esta es una aplicación para encontrar una estación de bicimad o bicipark dependiendo del Edificios de carácter monumental en el que estes.' )
+    help_message ='Debes introducir un Edificios de carácter monumental de Madrid' 
+    parser.add_argument('-e', '--edificio', help=help_message, type=str)
+    args = parser.parse_args()
+    #return args.e
+    
+    if args.e == 'bicimad':
+        bicimad(args.e)
+    elif args.e == 'bicipark':
+        bicipark(args.e)
     else:
-        result = 'FATAL ERROR...you need to select the correct method'
-    print(f'The result is => {result}')
+        print('Este Edificios de carácter monumental no esta. Prueba con otro.')
+    
+    
